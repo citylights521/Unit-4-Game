f845c91f89450b70549a62d8937d440911d04ba6
CGibeau <claire.gibeau@outlook.com>
2019-03-05 17:11:29 -0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Unit-4-Game
+# Bob's Burger Maker
+
+The dinner rush has arrived and Bob needs your help getting orders out. Tie on your apron, grab your hairnet, and get ready to start flipping burgers!
+
+##How to play: 
+When the page downloads, a random ‘Orders Needed’ number will generate. This is where Bob needs your help; there are four meal items which will each have a random value assigned. When you click each item, a corresponding value will display under ‘Orders Fulfilled’. Your mission is to click the right combination of food items to equal the exact number of orders needed. Match the number exactly to achieve an order up win! Go over and you lose. One a win or lose event has occured, a new Orders Needed number will populate, the Orders Fulfilled reset back to 0, and the food item values change.
+
+##Components used: 
+HTML, CSS, JavaScript, jQuery, Bootstrap, GoogleFonts
+
+##Author: 
+Claire Gibeau
+
+##Feedback: 
+I approached this game a little differently than the last game I built (hangman). In hangman I first built out the functionality and then added the aesthetics after words. After hearing how some of my classmates were approaching the assignment (shout out to them for sharing their strategy!), I decided to mix it up and start with the layout, general theme, and some design, and then to plug in the functionality where it belonged. I liked approaching building a game this way much better. I found it made creating naming conventions much easier, and overall created more synergy and understanding when it came to building the JavaScript logic. I started to play with audio on this game but left it as just the theme song playing vs adding win/lose sound effects; excited to spend more time on that and other enhancements. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,6 @@ var buttonCokeValue;
 var winValue = 0;
 var loseValue = 0;
 
-// initiate game
-
-// // random order number generator
-
-
-
-// function myFunction() {
-//     var i = Math.floor((Math.random() * ))
-
-// }
-
 
 //random orders needed number
 $(document).ready(function () {
@@ -43,6 +32,10 @@ function newGame() {
   printOrdersFulfilled();
   printScoreboard();
 
+  console.log("Burger Value: ", buttonBurgerValue);
+  console.log("Milkshake Value: ", buttonMilkshakeValue);
+  console.log(buttonFriesValue);
+  console.log(buttonCokeValue);
 }
 
 
@@ -58,12 +51,14 @@ $("#milkshakeButton").on("click", function () {
   ordersFulfilled = buttonMilkshakeValue + ordersFulfilled;
   printOrdersFulfilled();
   checkOrders();
+ 
 });
 
 $("#friesButton").on("click", function () {
   ordersFulfilled = buttonFriesValue + ordersFulfilled;
   printOrdersFulfilled();
   checkOrders();
+
 });
 
 $("#cokeButton").on("click", function () {
@@ -98,7 +93,6 @@ function checkOrders() {
   }
 }
 
-
 //diplay wins, losses, and message
 function printScoreboard() {
   $("#wins").text(winValue);
@@ -108,143 +102,3 @@ function printScoreboard() {
 function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
-
-
-
-
-  // function .click(burgerButton) {
-  //   ordersFulfilled = getRandomArbitrary(1, 12);
-  //   // $("#ordersFilled").click(burgerButton);
-  // }
-
-
-//       <script>
-
-//          $(document).ready(function($){
-
-//           $('button').append("<img src=poweroff.png />").button();
-
-//         });
-//       </script>
-
-//   </head> 
-//   <body> 
-//     <p>Switch off the Device</p>
-//   <button id="btn1"></button> 
-//   </body>
-//   </html> -->
-
-//     $(".burgerButton").mousedown(function () {
-//         $(this).css("border", "1px solid #ECECEC");
-//     }).mouseup(function () {
-//         $(this).css("border", "none");
-//     })
-// })
-
-// $("#burgerButton").click(function(){
-//     alert("image is clicked");  
-
-// }); 
-
-
-
-
-// //global variables
-// var word;
-// var numberWins = 0;
-// var numberLosses = 0;
-// var lettersRemainingCount;
-// var answers;
-// var guessedLetters;
-// var remainingGuesses;
-// var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var words = ["beach", "boardwalk", "starfish", "swimming", "sunscreen", "waves", "lounging", "sunshine", "happy", "sand", "surfing", "shells", "fish"];
-
-// //initiate game
-// function startGame() {
-//     document.getElementById("wins").textContent = numberWins;
-//     document.getElementById("losses").textContent = numberLosses;
-
-//     //pick random word
-//     word = words[Math.floor(Math.random() * words.length)];
-
-//     //answers array
-//     answers = [];
-//     for (var i = 0; i < word.length; i++) {
-//         answers[i] = "_";
-//     }
-
-//     //guessed array
-//     guessedLetters = [];
-//     document.getElementById("guess").textContent = guessedLetters;
-
-
-//     //6 guesses allowed per game/round
-//     remainingGuesses = 6;
-//     document.getElementById("turnsLeft").textContent = remainingGuesses;
-
-//     //guessing word platform _ _ _
-//     document.getElementById("display").textContent = answers.join(" ");
-
-//     //player pushes keys to start guessing
-//     document.onkeyup = onKeyPressed;
-
-//     //letters guessed this round outside of the correct letters in the current word
-//     lettersRemainingCount = word.length;
-// }
-
-// //event callback for keyboard press
-// function onKeyPressed(event) {
-//     var guess = event.key.toLowerCase();
-
-//     if (letters.includes(guess) === false) {
-//         return;
-//     }
-
-//     document.getElementById("log").textContent = null;
-
-//     //guess log for debugging 
-//     // document.getElementById("log").textContent = guess;
-
-//     var correctLetter = false;
-
-//     if (guess.length !== 1) {
-//         console.log("Nice try-single letters only!");
-
-//     } else {
-//         for (var j = 0; j < word.length; j++) {
-//             if (word[j] === guess) {
-//                 correctLetter = true;
-//                 if (answers[j] !== guess) {
-//                     answers[j] = guess;
-//                     lettersRemainingCount--;
-//                 }
-//             }
-//         }
-//     }
-
-//     document.getElementById("display").textContent = answers.join(" ");
-
-//     if (correctLetter === false && guessedLetters.includes(guess) === false) {
-//         guessedLetters.push(guess);
-//         remainingGuesses--;
-//         document.getElementById("turnsLeft").textContent = remainingGuesses;
-//     }
-//     document.getElementById("guess").textContent = guessedLetters;
-
-//     if (lettersRemainingCount <= 0) {
-//         numberWins++;
-//         var logDiv = document.getElementById("log");
-//         logDiv.textContent = "Congrats! You guessed the word " + word + "!";
-//         logDiv.style.color = "green";
-//         startGame();
-//     }
-
-//     if (remainingGuesses <= 0) {
-//         numberLosses++;
-//         var logDiv = document.getElementById("log");
-//         logDiv.textContent = "Good Game (but you loose...). Try again!";
-//         logDiv.style.color = "red";
-//         startGame();
-//     }
-// }
